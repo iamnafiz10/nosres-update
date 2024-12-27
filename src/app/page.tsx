b0aa3b696f49d1a3f25f6cf7b76413e87ced79a6
Roshan SaiHam Nafiz <roshansaihamnafiz@gmail.com>
2024-12-27 21:36:31 +0600

    dsd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
-import Header from './components/Header';
+// import Header from './components/Header';
 import {BrowserRouter} from 'react-router-dom'
 
 export default function Home() {
     return (
         <>
             <BrowserRouter>
-                <Header/>
+                <div>
+                    hello
+                </div>
             </BrowserRouter>
         </>
     );
