4cee29359a70d58c226cee2189be93f8bedd07cb
Roshan SaiHam Nafiz <roshansaihamnafiz@gmail.com>
2024-12-27 22:44:31 +0600

    Maybe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-"use client"
-import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Homepage from './pages/home/page'
 import Header from "@/app/components/header/page";
 
 export default function Home() {
     return (
         <>
-            <BrowserRouter>
-                <Header/>
-                <Routes>
-                    <Route path="/" element={<Homepage/>}/>
-                </Routes>
-            </BrowserRouter>
+            <Header/>
+            <Homepage/>
         </>
     );
 }
